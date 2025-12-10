e973024dc9e342c744ab1d8dd6766912fc6bdf3b
EsmeMejia29 <esmejia2908@gmail.com>
2025-12-10 15:23:26 -0600

    c10

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,5 @@
-import { supabase } from './config.js';
-import { abrirModal, cerrarModal } from './ui.js';
+import { supabase } from '../js/config.js';
+import { abrirModal, cerrarModal } from '../js/ui.js';
 
 let idPedidoActual = null;
 let costoEnvioActual = 0;
