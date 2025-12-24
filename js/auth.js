698691661b6df67ba9c25079bbc245600af07019
EsmeMejia29 <esmejia2908@gmail.com>
2025-12-23 23:54:49 -0600

    Corrección de error

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,17 @@
-const SUPABASE_URL = 'https://fpmsddnonhiqxnsydfpz.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwbXNkZG5vbmhpcXhuc3lkZnB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5MTE4NTAsImV4cCI6MjA3ODQ4Nzg1MH0.Lj3q5iOHpGzBhwul1yPx4jxoSB9u-blu5EYJ6lsftXY';
+var SUPABASE_URL = 'https://fpmsddnonhiqxnsydfpz.supabase.co';
+var SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwbXNkZG5vbmhpcXhuc3lkZnB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5MTE4NTAsImV4cCI6MjA3ODQ4Nzg1MH0.Lj3q5iOHpGzBhwul1yPx4jxoSB9u-blu5EYJ6lsftXY';
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+//const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+if (typeof supabase === 'undefined') {
+    window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+} else {
+    if (!window.supabase.auth) {
+        window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+    }
+}
+
+var supabase = window.supabase;
 
 function estandarizarTelefono(telefono) {
     const telLimpio = telefono.replace(/[^\d]/g, '');
