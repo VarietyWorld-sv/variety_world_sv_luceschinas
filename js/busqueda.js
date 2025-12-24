603e0664583841be9f136d7aa846dc5f16aa9ba2
EsmeMejia29 <esmejia2908@gmail.com>
2025-12-24 00:33:50 -0600

    Cambios menores

--- js/busqueda.js
+++ js/busqueda.js
@@ -1,22 +1,13 @@
 //import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 if (typeof SUPABASE_URL === 'undefined') {
-
     var SUPABASE_URL = 'https://fpmsddnonhiqxnsydfpz.supabase.co';
-
     var SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwbXNkZG5vbmhpcXhuc3lkZnB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5MTE4NTAsImV4cCI6MjA3ODQ4Nzg1MH0.Lj3q5iOHpGzBhwul1yPx4jxoSB9u-blu5EYJ6lsftXY';
-
 }
-
-
 
 if (!window.supabaseClient) {
-
     window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
 }
-
-
 
 var supabase = window.supabaseClient;
 
